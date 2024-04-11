3de91aa35087f46ead8710f042340688d688371c
Heydeaddad <166680987+Heydeaddad@users.noreply.github.com>
2024-04-11 06:49:01 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-# Load your dataset
-df = pd.read_excel("C:\\Users\\Oleg\\OneDrive\\Рабочий стол\\Новая папка\\trimmed_dataset.xlsx")  # Make sure to update this path to the location of your dataset
+# Loading
+df = pd.read_excel("path to trimmed")  
 
-# Count unique video IDs
-num_nodes = df['id'].nunique()
+num_nodes = df['id'].nunique() # counting
 
 print("Number of nodes (unique video IDs):", num_nodes)
